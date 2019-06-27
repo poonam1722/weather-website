43e343ce1d776b3e14927b783f2b0d48ddd3c65b
poonam1722 <you@example.com>
2019-06-27 15:34:49 +0530

    Init commit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast=(x,y,callback)=>{
             callback('problem in service',)
         }
         else{
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipIntensity} chance of rain`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipIntensity} chance of rain. Humidity is ${body.daily.data[0].humidity}`)
         }
     })
 }
